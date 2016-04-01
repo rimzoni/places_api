bbeb9195c924b9f890e8691e998c654615d6b2f4
rimzoni <rimzoni@gmail.com>
2016-04-01 21:56:59 +0200

    adding routes

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 
 var app = express();
 
+var placesRouter = express.Router();
+
+placesRouter.route('/Places')
+		.get(function(req,res){
+			var responseJson = {place: "Places api"};
+			res.json(responseJson);
+		});
+
+app.use('/api',placesRouter);
+
 app.use('/', function(req,res){
 	res.send('Welcome to my API!');
 });
