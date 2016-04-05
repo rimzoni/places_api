2f5caad59b5e31751c0df8c830950a9c7d05c793
rimzoni <rimzoni@gmail.com>
2016-04-05 22:19:34 +0200

    refactoring routes

---
+++ routes/placeRoutes.js
@@ -0,0 +1,43 @@
+var express = require('express');
+
+var routes = function(Place){
+
+  var placesRouter = express.Router();
+  
+  placesRouter.route('/')
+  		.post(function(req,res){
+  			var place = new Place(req.body);
+
+  			place.save();
+
+  			res.status(201).send(place);
+  		})
+  		.get(function(req,res){
+
+  			var query = {};
+  			if(req.query.name){
+  				query.name = req.query.name;
+  			}
+
+  			Place.find(query,function(err,places){
+  				if(err)
+  					res.status(500).send(err);
+  				else
+  					res.json(places);
+  			})
+  		});
+
+  placesRouter.route('/:placeId')
+  		.get(function(req,res){
+
+  			Place.findById(req.params.placeId,function(err,place){
+  				if(err)
+  					res.status(500).send(err);
+  				else
+  					res.json(place);
+  			})
+  });
+return placesRouter;
+};
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -8,44 +8,11 @@ var Place = require('./models/placeModel');
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
-var placesRouter = express.Router();
 
-placesRouter.route('/Places')
-		.post(function(req,res){
-			var place = new Place(req.body);
-
-			place.save();
-
-			res.status(201).send(place);
-		})
-		.get(function(req,res){
-
-			var query = {};
-			if(req.query.name){
-				query.name = req.query.name;
-			}
-
-			Place.find(query,function(err,places){
-				if(err)
-					res.status(500).send(err);
-				else
-					res.json(places);
-			})
-		});
-
-placesRouter.route('/Places/:placeId')
-		.get(function(req,res){
-
-			Place.findById(req.params.placeId,function(err,place){
-				if(err)
-					res.status(500).send(err);
-				else
-					res.json(place);
-			})
-		});
+placesRouter = require('./routes/placeRoutes')(Place);
 
 
-app.use('/api',placesRouter);
+app.use('/api/places',placesRouter);
 
 app.use('/', function(req,res){
 	res.send('Welcome to my API!');
