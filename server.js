5d7891b7afd7d48b3f6737bf5fe3da549fddafe5
rimzoni <rimzoni@gmail.com>
2016-04-09 23:50:37 +0200

    adding filter handling in server.js

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ var express = require('express');
 var db = mongoose.connect('mongodb://localhost/placeAPI');
 
 var Place = require('./models/placeModel');
+var Filter = require('./models/filterModel');
+
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 placesRouter = require('./routes/placeRoutes')(Place);
+filterRouter = require('./routes/filterRoutes')(Filter);
 
 
 app.use('/api/places',placesRouter);
+app.use('/api/filters',filterRouter);
 
 app.use('/', function(req,res){
 	res.send('Welcome to places API!');
